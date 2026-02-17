1c7f7c6af8b3e73f773fae393c8761fb486de40d
sunithra <sunithra.chidhambara@aspiresys.com>
2026-02-17 09:39:40 +0530

    Replace the mock data with mock api

--- src/App.js
+++ src/App.js
@@ -8,11 +8,35 @@ export default function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    setTimeout(() => {
-      setProducts(apiData);
-    }, 500);
+    fetchProducts();
   }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(
+        "https://mocki.io/v1/264df412-a0a5-4d4b-9ac4-6e42d4a243c4"
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+
+      setProducts(data);
+    } catch (err) {
+      setError("Unable to load products");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <h2>Loading products...</h2>;
+  if (error) return <h2>{error}</h2>;
 
   const addToCart = (item) => {
     const totalQty = cart.length;
